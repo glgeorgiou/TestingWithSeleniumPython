bb3e400019af064c04fe5c25168aa26edb780c70
George <ggeorgiou.gr@gmail.com>
2023-12-14 14:44:40 +0200

    Firefox webdriver

---
+++ 1-UI-Elements/driver-browsers.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+
+"""
+#Using Chrome
+cDriver = webdriver.Chrome()
+cDriver.get('https://the-internet.herokuapp.com/')
+print('Site title:', cDriver.title)
+# cDriver.close()
+"""
+#TODO - Issue: Chrome closes automaticaly even though you don't close it by using code
+
+"""
+"""
+# Using Firefox
+fDriver = webdriver.Firefox()
+fDriver.get('https://the-internet.herokuapp.com/')
+
+# fDriver.close()
