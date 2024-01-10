a412be7ff827a77530b97c010dd08493f72aa7b3
George <ggeorgiou.gr@gmail.com>
2024-01-10 16:54:20 +0200

    Testing basic UI.

--- 1-UI-Elements/inputs.py
+++ 1-UI-Elements/inputs.py
@@ -3,13 +3,13 @@ from selenium.webdriver.common.by import By
 
 # Firefox webdriver for herokuapp site
 fDriverHA = webdriver.Firefox()
-"""
 fDriverHA.get('https://the-internet.herokuapp.com/')
 
 # Click on inputs link
 fDriverHA.find_element(By.CSS_SELECTOR, "a[href='/inputs']").click()
 # fDriverHA.find_element((By.CSS_SELECTOR, "input[type='number']")).send_keys(7) # TODO: Error message in internal file
 
-"""
+
 
 # Enter values in https://rahulshettyacademy.com/angularpractice/
+
