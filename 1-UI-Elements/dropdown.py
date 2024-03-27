d947af3b9f5884186bbcb10b5d99ac1f61b1ff23
George <ggeorgiou.gr@gmail.com>
2024-03-27 14:01:06 +0200

    Testing drop down menu

---
+++ 1-UI-Elements/dropdown.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support.select import Select
+
+# Firefox webdriver for herokuapp site
+fDriverHA = webdriver.Firefox()
+fDriverHA.get('https://the-internet.herokuapp.com/')
+
+# Click on drop down link
+fDriverHA.find_element(By.CSS_SELECTOR, "a[href='/dropdown']").click()
+
+# Drop down list and select items
+dropdown = Select(fDriverHA.find_element(By.ID, "dropdown"))
+dropdown.select_by_index(1)
+dropdown.select_by_index(2)
+# NOTE: to be continue at https://elementalselenium.com/tips/5-select-from-a-dropdown
