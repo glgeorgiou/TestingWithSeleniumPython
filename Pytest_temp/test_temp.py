ed89a766e053cf0656a7bde75e9d725193a001d6
George <ggeorgiou.gr@gmail.com>
2024-11-14 13:15:10 +0200

    Experimenting pytest

--- Pytest_temp/test_temp.py
+++ Pytest_temp/test_temp.py
@@ -1,5 +1,14 @@
+import pytest
+
+
 def test_SayHello():
     print('Hello there')
 
+
+# @pytest.Mark.smoke # ToDo - Mark method does not have smoke and other attributes. Study pytest.
 def test_bool():
-    assert 2>3
+    assert 4>3
+
+
+def test_book2():
+    assert -1<1
